66d0556a5267f08519799ba30f84524997347be1
liangzhongtai <liangzhongtai2008@126.com>
2017-01-13 11:11:20 +0800

    添加可自定义字体的TextView子类，其他Button等的TextView子类也可以使用类似的方式改变字体。

---
+++ app/src/main/java/juzi/com/replacefontsdemo/view/FontFreeTextView.java
@@ -0,0 +1,44 @@
+package juzi.com.replacefontsdemo.view;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import juzi.com.replacefontsdemo.view.global.Global;
+import juzi.com.replacefontsdemo.view.global.Utils;
+
+
+/**
+ * Created by liangzhongtai on 2017/1/12.
+ */
+public class FontFreeTextView extends TextView {
+    protected static Typeface typeface;
+    protected static String fontAssetPath;
+    public FontFreeTextView(Context context) {
+        this(context, null);
+    }
+    public FontFreeTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+    public FontFreeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    public void setFontPath(String fontAssetPath){
+        if(typeface==null||this.fontAssetPath==null||!this.fontAssetPath.equals(fontAssetPath)) {
+            typeface = Utils.getTypeface(fontAssetPath);
+            this.fontAssetPath = fontAssetPath;
+        }
+        setTypeface(typeface);
+        invalidate();
+    }
+
+
+
+}
